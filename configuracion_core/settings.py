c1a82e9b81870756ad703d862a0c1cb4bb7f15eb
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2026-07-07 00:50:21 -0500

    Fix: Añadiendo configuración de PostgreSQL para producción

--- configuracion_core/settings.py
+++ configuracion_core/settings.py
@@ -87,6 +87,14 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
